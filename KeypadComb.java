878071039a9f7d1d350c9047684dfdd545d98bba
skkhokhar787 <skkhokhar787@gmail.com>
2025-06-29 12:11:55 +0500

    Add KeypadComb code

--- KeypadComb.java
+++ KeypadComb.java
@@ -1,11 +1,15 @@
 public class KeypadComb {
+
+    // This array maps the digits to their corresponding characters on a phone keypad
     public static String [] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
     public static void printKeypadComb (String str, int idx, String combination) {
+        // Base case: if we reach the end of the string
         if (idx == str.length()) {
             System.out.println(combination);
             return;
         }
-
+        // main code
         char currentChar = str.charAt(idx);
         String mapping = keypad[currentChar - '0'];
         for (int i = 0; i < mapping.length(); i++) {
